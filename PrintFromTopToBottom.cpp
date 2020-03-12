7bae7d7a3076ead9c850d758d03dbc9e3f27895a
liang xu <15921991927@163.com>
2020-03-12 16:06:22 +0800

    update

--- PrintFromTopToBottom.cpp
+++ PrintFromTopToBottom.cpp
@@ -14,28 +14,6 @@
 #include "utils/Tree.h"
 using namespace std;
 
-char* str="abd##e##cf##g";
-int array[13]={1,2,4,0,0,5,0,0,3,6,0,0,7};
-int length=13;
-int current=0;
-TreeNode* createTree(){
-    TreeNode* node;
-    if(length>current){
-        if(str[current]!='#'){
-            node=createTreeNode(str[current],array[current]); 
-            current+=1;
-            node->left=createTree();
-            current+=1;
-            node->right=createTree();
-        }else{
-            return NULL;
-        }       
-    }else{
-        return NULL;
-    }
-    return node;
-}
-
 vector<int> PrintFromTopToBottom(TreeNode* root) {
     vector<int> result;
     vector<TreeNode*> sequence;
@@ -58,7 +36,6 @@ vector<int> PrintFromTopToBottom(TreeNode* root) {
 }
 
 int main(){
-    TreeNode* root=createTree();
     traveralTree(root);
     vector<int> result=PrintFromTopToBottom(root);
     for(int i=0;i<result.size();i++){
